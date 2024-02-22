609a97c80e863037e3fc6255456d762344cc4c63
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-02-22 08:24:02 +0200

    Initial Starter files

---
+++ gilded-rose-swift/GildedRose/GildedRose.swift
@@ -0,0 +1,66 @@
+//
+//  GildedRose.swift
+//  GildedRose
+//
+//  Created by Tom Heinan on 8/31/15.
+//  Copyright © 2015 Tom Heinan. All rights reserved.
+//
+
+public class GildedRose {
+
+    public static func updateQuality(items: Array<Item>) -> [Item] {
+        var items = items
+        for i in 0 ..< items.count {
+            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+                if items[i].quality > 0 {
+                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
+                        items[i].quality = items[i].quality - 1
+                    }
+                }
+            } else {
+                if items[i].quality < 50 {
+                    items[i].quality = items[i].quality + 1
+                    
+                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+                        if items[i].sellIn < 11 {
+                            if items[i].quality < 50 {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                        
+                        if items[i].sellIn < 6 {
+                            if items[i].quality < 50 {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if items[i].name != "Sulfuras, Hand of Ragnaros" {
+                items[i].sellIn = items[i].sellIn - 1
+            }
+            
+            if items[i].sellIn < 0 {
+                if items[i].name != "Aged Brie" {
+                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+                        if items[i].quality > 0 {
+                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
+                                items[i].quality = items[i].quality - 1
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
+                    }
+                } else {
+                    if items[i].quality < 50 {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+            }
+        }
+        
+        return items
+    }
+    
+}
